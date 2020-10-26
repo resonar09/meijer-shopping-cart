fe544f3bbe6cc51f7542d99acda9127af6e41d68
resonar09 <darrenperez@gmail.com>
2020-10-26 11:58:05 -0400

    Added padding to the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
   //const { userData } = useContext(UserContext);
 
   return (
-    <Container>
+    <Container className="pt-3">
       <p>This simple shopping cart application was created by Darren Perez as a POC demo for the Meijer Company.</p>
       <p>Please visit my github repository for source code. </p> <a href="https://github.com/resonar09/meijer-shopping-cart" target="_blank" rel="noopener noreferrer">Git Hub Repo - meijer-shopping-cart</a>
     </Container>
